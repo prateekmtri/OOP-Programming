757dc02bb684e0bce083c7a245ae4f9e3d20e295
prateekmtri <prateek1tri2@gmail.com>
2026-02-03 14:58:04 +0530

    basic quetion

---
+++ 10.py
@@ -0,0 +1,10 @@
+class Name:
+    def __init__(self , name):
+        self.name = name
+        
+    def show_name(self):
+        print(f"My name is {self.name}")
+        
+obj = Name("Prateek")
+obj.show_name()            
+        
