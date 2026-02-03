218cdc14838ccc0b5b0dcdf6859473de7780206e
prateekmtri <prateek1tri2@gmail.com>
2026-02-03 15:05:24 +0530

    update

--- 10.py
+++ 10.py
@@ -1,10 +1,15 @@
 class Name:
-    def __init__(self , name):
+    def __init__(self , name, age):
         self.name = name
+        self.age = age
         
     def show_name(self):
         print(f"My name is {self.name}")
         
-obj = Name("Prateek")
-obj.show_name()            
+    def show_age(self):
+        print(f"i am {self.age} year old")    
+        
+obj = Name("Prateek",22)
+obj.show_name()
+obj.show_age()            
         
