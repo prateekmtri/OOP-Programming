463f0209f18d5f8ec8033e1df67c9d13e953c443
prateekmtri <prateek1tri2@gmail.com>
2026-02-04 15:02:23 +0530

    basic question

---
+++ 16.py
@@ -0,0 +1,23 @@
+class Student:
+    def __init__(self, name, marks, fees):
+        self.name = name
+        self._marks = marks
+        self.__fees = fees
+
+    def show_inside(self):
+        print("Inside class:")
+        print(self.name)
+        print(self._marks)
+        print(self.__fees)
+
+    def show_fees(self):      # ✅ inside class
+        print(self.__fees)
+
+
+obj = Student("Prateek", 80, 5000)
+
+obj.show_inside()
+print(obj.name)
+print(obj._marks)
+
+obj.show_fees()   # ✅ access private correctly
