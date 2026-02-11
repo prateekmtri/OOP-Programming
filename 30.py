006775feb99f985ff0eca34071092e8ff23faca4
prateekmtri <prateek1tri2@gmail.com>
2026-02-11 13:03:05 +0530

    completed first class

--- 30.py
+++ 30.py
@@ -1,11 +1,14 @@
 class Mobile:
-    def __init__(self , brand , battery ):
+    def __init__(self , brand  ):
         self.brand = brand
-        self.__battery = battery
+        self.__battery = 20
         
     def show_battery(self):
         print(f" Brand : {self.brand}")
         print(f"Battery : {self.__battery}")
         
-            
+    def charge(self , ammount):
+            self.__battery = self.__battery + ammount
+            print(f"New Battery : {self.__battery}")
+                    
         
