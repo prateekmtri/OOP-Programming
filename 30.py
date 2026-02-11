b9d41317601975a64546fa68beb2a576fbe49b28
prateekmtri <prateek1tri2@gmail.com>
2026-02-11 13:23:48 +0530

    very mini project

--- 30.py
+++ 30.py
@@ -11,4 +11,18 @@ class Mobile:
             self.__battery = self.__battery + ammount
             print(f"New Battery : {self.__battery}")
                     
+
+class Charger:
+    def charge_mobile(self , mobile_object , amount):
+        mobile_object.charge(amount)
         
+        
+        
+m = Mobile("vivo")
+c = Charger()
+
+m.show_battery()
+c.charge_mobile(m, 30)
+m.show_battery()
+   
+                
