c12ef2d1abe636b00d0a7a0f5b6823a0f97e755a
prateekmtri <prateek1tri2@gmail.com>
2026-04-03 19:59:18 +0530

    34 question

---
+++ 34.py
@@ -0,0 +1,96 @@
+class Doctor:
+    def __init__(self, doctor_id , name , specilization):
+        self.doctor_id = doctor_id
+        self.name = name
+        self.specilization = specilization
+        self.__is_available = True
+        
+    def show_doctor(self):
+        print(f"Doctor Id : {self.doctor_id}")
+        print(f"Doctor Name : {self.name}")
+        print(f"Specilization : {self.specilization}")
+        print(f"Doctor availability : {self.__is_available}")
+        
+    def assign_patient(self):
+        if self.__is_available == True:
+            self.__is_available = False
+            print(" Patient is assign")
+        else:
+            print("Doctor not available")
+            
+    def complete_checkup(self):
+        if self.__is_available ==False:
+            self.__is_available = True
+            print("checkup is complete")
+        else:
+            print("Busy")
+        
+        
+class Patient:
+    def __init__(self , patient_id , name ):
+        self.patient_id = patient_id
+        self.name =  name
+        self.doctor_assigned = []
+        
+    def show_patient_info(self):
+        print(f"Patient id : {self.patient_id}")
+        print(f"Patient Name : {self.name}")
+        print(f"Doctor Assigned to patience : {self.doctor_assigned}")
+        
+        
+class Hospital:
+    def __init__(self):
+        self.doctor = []
+        self.patience = []
+        
+    def add_doctor(self , doctor_object):
+        self.doctor.append(doctor_object)
+    
+    def add_patient(self, patient_object):
+        self.patience.append(patient_object)                  
+    
+    def assign_doctor(self , doctor_id , patient_id):
+        for d in self.doctor:
+            if d.doctor_id == doctor_id:
+                for p in self.patience:
+                    if p.patient_id == patient_id:
+                        d.assign_patient()
+                        p.doctor_assigned.append(d.name)
+                        print("Doctor assigned successfully")
+                        return
+        print("Doctor or Patient not found")
+        
+    def complete_checkup(self, doctor_id, patient_id):
+         for d in self.doctor:
+            if d.doctor_id == doctor_id:
+                for p in self.patience:
+                    if p.patient_id == patient_id:
+                        d.complete_checkup()
+                        if d.name in p.doctor_assigned:
+                            p.doctor_assigned.remove(d.name)
+                        print("Checkup completed")
+                        return
+         print("Doctor or Patient not found")
+         
+         
+         
+
+
+d1 = Doctor(1, "Dr. Sharma", "Cardiology")
+p1 = Patient(101, "Ravi")
+
+h = Hospital()
+h.add_doctor(d1)
+h.add_patient(p1)
+
+h.assign_doctor(1, 101)
+
+p1.show_patient_info()
+d1.show_doctor()
+
+h.complete_checkup(1, 101)
+
+p1.show_patient_info()
+d1.show_doctor()         
+                          
+                                    
